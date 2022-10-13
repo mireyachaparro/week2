2fa50cd88bafca65b524ef1f3164fe7fa3ea49cb
mireyachaparro <mireyacn99@gmail.com>
2022-10-13 12:07:42 +0200

    errores js

--- testing/sample.js
+++ testing/sample.js
@@ -1,10 +1,23 @@
 export function add(a, b) {
-    if (a === 0 || b === 0) {
-        return 'ERROR';
-    }
-    return a + b;
+    return +a + Number(b); //dos formas distintas que convertir algo a numero
 }
 
 export function subtraction(a, b) {
     return a - b;
+}
+
+export function division(a, b) {
+    const zeroError = new Error('ERROR, no se puede dividir entre 0');
+    const paramError = new Error('ERROR, parametro no valido');
+
+    if (b === 0) {
+        throw zeroError;
+    }
+    if (a === null || b === null) {
+        throw paramError;
+    }
+    if (isNaN(a) || isNaN(b)) {
+        throw paramError;
+    }
+    return a / b;
 }
--- testing/sample.test.js
+++ testing/sample.test.js
@@ -1,4 +1,4 @@
-import { add, subtraction } from './sample.js';
+import { add, division, subtraction } from './sample.js';
 
 describe('Testing add', () => {
     test('if a = 2 and b = 2 add should be', () => {
@@ -6,31 +6,67 @@ describe('Testing add', () => {
         let b = 2;
         expect(add(a, b)).toBe(4);
     });
+
     test('if a = 2 and b = 2 add should be', () => {
         let a = 2;
         let b = 2;
         expect(add(a, b)).toBeLessThan(5);
     });
-    test('if a = 0 should be ERROR', () => {
-        let a = 0;
-        let b = 2;
-        expect(add(a, b)).toBe('ERROR');
-    });
-    test('if b = 0 should be ERROR', () => {
-        let a = 8;
-        let b = 0;
-        expect(add(a, b)).toBe('ERROR');
-    });
 });
 
 describe('Testing subtraction', () => {
-    test('if a = 2 and b = 2 subtraction should be 0', () => {
+    test('if a = 2 and b = 2 result should be 0', () => {
         let a = 2;
         let b = 2;
         expect(subtraction(a, b)).toBe(0);
     });
 });
 
+describe('Testing division', () => {
+    test('if a = 4 and b = 2 return should be 2', () => {
+        let a = 4;
+        let b = 2;
+        expect(division(a, b)).toBe(2);
+    });
+    test('if b = 0 return should be error', () => {
+        let a = 2;
+        let b = 0;
+        expect(() => {
+            division(a, b);
+        }).toThrow();
+    });
+    test('if b = 0 return should be error', () => {
+        let a = null;
+        let b = 5;
+
+        try {
+            division(a, b);
+        } catch (e) {
+            expect(e.message).toBe('ERROR, parametro no valido');
+        }
+    });
+    test('if a = NaN return should be error', () => {
+        let a = 'pepe';
+        let b = 5;
+
+        try {
+            division(a, b);
+        } catch (e) {
+            expect(e.message).toBe('ERROR, parametro no valido');
+        }
+    });
+    test('if b = NaN return should be error', () => {
+        let a = 5;
+        let b = 'pepe';
+
+        try {
+            division(a, b);
+        } catch (e) {
+            expect(e.message).toBe('ERROR, parametro no valido');
+        }
+    });
+});
+
 //funcion expect: espero algo de lo que hay dentro
 
-//descripcion y funcion que se la pasas a otra funcion para que haga cosas
+//descripcion y funcion que se la pasas a otra funcion para que haga cosas.
